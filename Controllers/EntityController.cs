6cdaf7e8b7878608878945074d081686417564e7
Shikiju <koenvbaast25@gmail.com>
2012-09-29 00:54:18 +0200

    Initial import
    Initial import

---
+++ Controllers/EntityController.cs
@@ -0,0 +1,49 @@
+﻿namespace AngeronaSignum.WebApi.Controllers
+{
+    using AngeronaSignum.WebApi.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Http;
+    using Dapper;
+
+    public class EntityController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Entity> Index()
+        {
+            var entities = Enumerable.Empty<Entity>();
+            using (var connection = DatabaseService.GetOpenConnection())
+            {
+                entities = connection.Query<Entity>("select * from entity");
+            }
+            
+            return entities;
+        }
+
+        [HttpGet]
+        public Entity Index(int id)
+        {
+            var entity = new Entity;
+            using (var connection = DatabaseService.GetOpenConnection())
+            {
+                entity = connection.Query<Entity>("select * from entity where id = @id", new { id = id });
+            }
+
+            return entity;
+        }
+
+
+        [HttpPost]
+        public Entity Post(Entity entity)
+        {
+            using (var connection = DatabaseService.GetOpenConnection())
+            {
+                connection.Query<Entity>("INSERT INTO entity (name, email, password) VALUES (@name, @email,@password)", entity);
+            }
+
+            return entity;
+        }
+    }
+}
---
+++ DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿namespace AngeronaSignum
+{
+    using MySql.Data.MySqlClient;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Web;
+
+    public static class DatabaseService
+    {
+        public static readonly string connectionString = "Server=localhost;Database=angeronasignum.database.mysql;Uid=root;Pwd=lkk3v%99;";
+
+        public static MySqlConnection GetOpenConnection()
+        {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+    }
+}
