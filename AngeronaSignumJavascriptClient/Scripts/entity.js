8f3fceea555add3f0eacc2854aad00bd05aa6b42
Shikiju <koenvbaast25@gmail.com>
2012-10-17 22:41:42 +0200

    Ajax Requests
    started with ajax requests

--- AngeronaSignumJavascriptClient/Scripts/entity.js
+++ AngeronaSignumJavascriptClient/Scripts/entity.js
@@ -10,10 +10,20 @@ Person = Ember.Object.extend({
   // these will be supplied by `create`
   name: 'Koen',
   email: 'koenvbaast25@gmail.com',
-  alertMyName: function () {
-    alert(this.name);
+  password: 'koen25',
+  save: function () {
+    // assuming you are using jQuery, but could be other AJAX/DOM framework
+    $.post({
+      url: "http://localhost:50000/",
+      data: JSON.stringify(this.toArray()),
+      success: function (data) {
+        // your data should already be rendered with latest changes
+        // however, you might want to change status from something to "saved" etc.
+      }
+    });
   }
 });
+
 var view = Ember.View.create({
   templateName: 'mainEntity',
   entity: Person.create()
@@ -22,7 +32,8 @@ var view = Ember.View.create({
 view.entity.addObserver('name', function (a, b, c) {
   console.log(this.name);
   console.log(view.entity.get('name'));
-  this.whatmaname();
+  console.log(view.entity.get('email'));
+  console.log(view.entity.get('password'));
 })
 
 App.MyNameField = Ember.TextField.extend({
